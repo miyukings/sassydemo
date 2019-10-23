e16b3f7b3e1e57853f0bb87878aff6d24c65d1a2
miyuki Sekiguchi <miyukis3@hawaii.edu>
2019-10-22 15:19:26 -1000

    added js file

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+
+const mysitenav = document.querySelector('.site-header .site-nav');
+const mymenubutton = document.querySelector('.menu-button');
+
+mymenubutton.onclick = function () {
+    mysitenav.classList.toggle('active');
+}
