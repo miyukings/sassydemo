00b707b7da99216c54b2b60353cee4e918906fed
miyuki Sekiguchi <miyukis3@hawaii.edu>
2019-10-24 13:42:24 -1000

    added js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 
-
+// MOBILE NAV
 const mysitenav = document.querySelector('.site-header .site-nav');
 const mymenubutton = document.querySelector('.menu-button');
-
 mymenubutton.onclick = function () {
     mysitenav.classList.toggle('active');
 }
+
+// REMOVE ACTIVE CLASS WHEN USER CLICK ON OVERLAY NAV LINKS
+const mysitenavlinks = document.querySelectorAll('.site-header .site-nav a');
+for (var i = 0, len = mysitenavlinks.length; i < len; i++) {
+    mysitenavlinks[i].onclick = function () {
+        mysitenav.classList.remove('active');
+    };
+};
